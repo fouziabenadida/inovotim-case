fe10f6e1ef653077d123464ec598e80c46f3e0c3
fouziabenadida <benadiafouzia@gmail.com>
2022-09-07 15:07:09 +0300

    commit

---
+++ src/Components/Video-component/VideoComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+
+export default function VideoComponent() {
+  return (
+    <div>
+         
+      
+        <div>
+          <img src="" alt="" />
+        </div>
+        
+        <Button variant="primary">Go somewhere</Button>
+    
+    
+    </div>
+  )
+}
